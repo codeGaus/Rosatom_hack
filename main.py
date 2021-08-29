ce3868b3cf4f7689c18f95ac26165935bf5e2996
codeGaus <m2000863@edu.misis.ru>
2021-08-29 06:45:58 +0300

    Изменена структура параметров функции get_square_index

--- main.py
+++ main.py
@@ -102,11 +102,16 @@ def get_square_index(tag: int = 1,
                      point_1_lon: float = 142.745165,
                      point_2_lat: float = 46.952035,
                      point_2_lon: float = 142.756165,
-                     coefs=''):
-    k = json.loads(coefs)
+                     p1: float = 0.5, p2: int = 20,
+                     p3: float = 0.5, p4: int = 20,
+                     p5: float = 0.5, p6: int = 20,
+                     p7: float = 0.5, p8: int = 20,):
+
     dic = {}
-    for i in k.values():
-        dic[int(i[0])] = [int(i[1][0]), int(i[1][1])]
+    dic[0] = [p1, p2]
+    dic[1] = [p3, p4]
+    dic[2] = [p5, p6]
+    dic[3] = [p7, p8]
     point_1 = Point(point_1_lat, point_1_lon)
     point_2 = Point(point_2_lat, point_2_lon)
     info = square_business_index(tag, point_1, point_2, dic)
